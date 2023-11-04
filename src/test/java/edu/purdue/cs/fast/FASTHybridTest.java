b46b2ba5bd56e70a009f837633dcddf34d8e7f99
ThatUdeshUK <mail@udesh.xyz>
2023-11-03 23:05:38 -0400

    Updated tests

--- src/test/java/edu/purdue/cs/fast/FASTHybridTest.java
+++ src/test/java/edu/purdue/cs/fast/FASTHybridTest.java
@@ -7,17 +7,26 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 
-class FASTKNNTest {
+class FASTHybridTest {
     private final List<Query> queries;
     private final List<DataObject> objects;
     private final List<List<Integer>> answers;
 
-    public FASTKNNTest() {
+    public FASTHybridTest() {
         this.queries = List.of(
-                new KNNQuery(1, List.of("k1", "k2"), new Point(5.0, 5.0), 3, null,100),
-                new KNNQuery(2, List.of("k1", "k2"), new Point(7.0, 7.0), 2, null, 100),
-                new KNNQuery(3, List.of("k1"), new Point(8.0, 5.0), 3, null,100),
-                new KNNQuery(4, List.of("k2", "k3"), new Point(1.0, 7.0), 2, null, 100)
+                new MinimalRangeQuery(1, List.of("k1", "k2"), new Rectangle(.0, .0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(2, List.of("k1", "k2"), new Rectangle(.0, .0, 8.0, 10.0), null, 100),
+                new MinimalRangeQuery(3, List.of("k1", "k2"), new Rectangle(.0, .0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(4, List.of("k3", "k6"), new Rectangle(.0, .0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(5, List.of("k1", "k3"), new Rectangle(.0, .0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(6, List.of("k1", "k2", "k3"), new Rectangle(.0, .0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(7, List.of("k2", "k3", "k7"), new Rectangle(.0, .0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(8, List.of("k2"), new Rectangle(.0, .0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(9, List.of("k1", "k3"), new Rectangle(6.0, 7.0, 10.0, 10.0), null, 100),
+                new MinimalRangeQuery(10, List.of("k1", "k3"), new Rectangle(.0, .0, 4.0, 4.0), null, 100),
+                new MinimalRangeQuery(11, List.of("k1", "k3"), new Rectangle(6.0, .0, 10.0, 4.0), null, 100),
+                new KNNQuery(12, List.of("k1", "k2"), new Point(5.0, 5.0), 3, null,100),
+                new KNNQuery(14, List.of("k1", "k2"), new Point(7.0, 7.0), 2, null, 100)
         );
 
         this.objects = List.of(
@@ -29,11 +38,11 @@ class FASTKNNTest {
         );
 
         this.answers = List.of(
-                List.of(1, 2, 3),
-                List.of(1, 2, 3),
-                List.of(1, 3),
-                List.of(),
-                List.of(1, 2, 3)
+                List.of(1, 2, 3, 8, 12, 14),
+                List.of(1, 2, 3, 8, 12, 14),
+                List.of(1, 2, 3, 8, 12),
+                List.of(1, 2, 3, 8),
+                List.of(1, 2, 3, 8, 12, 14)
         );
     }
 
@@ -75,9 +84,6 @@ class FASTKNNTest {
         for (int i = 0; i < answers.size(); i++) {
             List<Integer> fastAns = testFAST.searchQueries(objects.get(i)).stream().map((Query query) -> query.id).toList();
             System.out.println(fastAns + " | " + answers.get(i).toString());
-
-            testFAST.printIndex();
-            System.out.println("------------\n");
 
             Assertions.assertArrayEquals(fastAns.stream().sorted().toArray(), answers.get(i).toArray());
         }
